929ff1ab2411087e8bfce5fb6f180e9b6d261d76
abdullahinabeel <134003913+abdullahinabeel@users.noreply.github.com>
2024-01-10 21:37:36 +0100

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://dev-hausatube.pantheonsite.io/graphql"
+	const endpoint = "https://hausa.lovestoblog.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://dev-hausatube.pantheonsite.io/` + encodeURI(path as string)
+					`https://hausa.lovestoblog.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
